fd833218c76b9ef1d4f1aac46ddca5dca8e06338
ParkChaeHun <pch3482@naver.com>
2025-12-05 12:09:37 +0900

    .

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
 
-from .views import RegisterView, LoginView, RefreshView, LogoutView, UserInfoView
+from .views import RegisterView, LoginView, UserInfoView, UpdateLikeView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view()),   
-    path("login/", LoginView.as_view()),         
-    path("refresh/", RefreshView.as_view()),     
-    path("logout/", LogoutView.as_view()),       
-    path("userinfo/", UserInfoView.as_view()),  
-    path("save_score/", views.save_score), 
+    path("register/", RegisterView.as_view()),      # POST 회원가입
+    path("login/", LoginView.as_view()),            # POST 로그인    
+    path("userinfo/", UserInfoView.as_view()),      # GET 사용자 정보, PATCH 수정
+    path("like/<int:movie_id>/", UpdateLikeView.as_view()), # POST 영화 찜, DELETE 취소
 ]
